d486290d8de93b734bdca3efde1e06fbc2d8732d
Santiago <104654708+SantiagoRuizM@users.noreply.github.com>
2024-08-21 15:12:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
 import os
 import json
 
+class Blockchain:
+    def __init__(self):
+        self.cadena = [self.crear_bloque_genesis()]
+        self.dificultad = 4  # Dificultad del Proof of Work
+
+    def crear_bloque_genesis(self):
+        return Bloque(0, "Bloque Génesis", time.time(), "0")
+
+    def obtener_ultimo_bloque(self):
+        return self.cadena[-1]
+
+    def agregar_bloque(self, nuevas_transacciones):
+        ultimo_bloque = self.obtener_ultimo_bloque()
+        nuevo_bloque = Bloque(len(self.cadena), nuevas_transacciones, time.time(), ultimo_bloque.hash)
+        nuevo_bloque.minar_bloque(self.dificultad)
+        self.cadena.append(nuevo_bloque)
+
+    def es_valida(self):
+        for i in range(1, len(self.cadena)):
+            bloque_actual = self.cadena[i]
+            bloque_anterior = self.cadena[i - 1]
+            
+            if bloque_actual.hash != bloque_actual.calcular_hash():
+                return False
+            
+            if bloque_actual.hash_anterior != bloque_anterior.hash:
+                return False
+            
+        return True
+
 class Tarea:
     def __init__(self, nombre, completada=False):
         self.nombre = nombre
